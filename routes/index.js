d201227e4b20c7b25ece76caac94c2328aa38aad
chip22294 <chip22294@gmail.com>
2023-05-08 19:53:55 +0300

    Some changes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-const express = require("express");
-const router = express.Router();
-const hbs = require("hbs");
+const express = require('express')
+const router = express.Router()
+const hbs = require('hbs')
 
-router.get("/", function (req, res, next) {
-  res.render("template", { name: "Hello world!" });
-});
+router.get('/', function (req, res, next) {
+  res.render('template', { name: 'Hello world!' })
+})
 
-module.exports = router;
+module.exports = router
